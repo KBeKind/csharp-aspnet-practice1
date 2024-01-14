2e373f9b58e84a75dadf2dbe4b45755ea69e7f21
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-01-14 16:34:30 -0600

    commit

---
+++ core-web-app1/Models/NewCustomer.cs
@@ -0,0 +1,11 @@
+﻿namespace core_web_app1.Models
+{
+    public class NewCustomer
+    {
+
+        public int CustomerId { get; set; }
+        public string? CustomerName { get; set; }
+        public double CustomerAmount { get; set; }    
+
+    }
+}
